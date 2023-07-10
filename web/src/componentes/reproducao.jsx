14b714bdc387afc94d85f2841f6641eec4d59c70
Aristides <harry.matoca@gmail.com>
2023-07-11 00:20:04 +0100

    Updates

--- web/src/componentes/reproducao.jsx
+++ web/src/componentes/reproducao.jsx
@@ -7,6 +7,8 @@ import { MdVolumeUp as Volume, MdPlayCircle as Play, MdOutlineFileDownload as Do
 import React, {useState, useEffect} from 'react'
 //import firebase from 'firebase/app';
 import firebase from 'firebase';
+import axios from 'axios'
+import 'firebase/storage'
 
 export default function Reproducao({ mediaSelecionado, pausar, reproduzir, avancar, retroceder, isPlaying, mediaRef }){
 
@@ -87,16 +89,58 @@ export default function Reproducao({ mediaSelecionado, pausar, reproduzir, avanc
     };
 
     const fazerDownload = async (mediaSelecionado) => {
-
+        console.log(mediaSelecionado.audioURL)
         try {
-            const storageRef = firebase.storage().ref();
+            /*const storageRef = firebase.storage().ref();
             const fileRef = storageRef.child(mediaSelecionado.audioURL);
 
             const url = await fileRef.getDownloadURL();
+            console.log('Link 2 ' + url)
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = `${mediaSelecionado.legenda} - ${mediaSelecionado.titulo}`;
+            link.click();*/
+
+            /*const response = await fetch(mediaSelecionado.url);
+            const blob = await response.blob();
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = mediaSelecionado.nome + ' - ' + mediaSelecionado.titulo;
+            link.click()
+
+
+            const url = mediaSelecionado.audioURL;
+            console.log('Link 2: ' + url);
+
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = `${mediaSelecionado.legenda} - ${mediaSelecionado.titulo}`;
+            link.setAttribute('download', '')
+            link.click();
+
+
+            const url = mediaSelecionado.audioURL;
+            console.log('Link 2: ' + url);
+
+            const response = await fetch(url);
+            const blob = await response.blob();
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = `${mediaSelecionado.legenda} - ${mediaSelecionado.titulo}`;
+            link.click();*/
+            const response = await axios({
+                url: mediaSelecionado.audioURL,
+                method: 'GET',
+                responseType: 'blob',
+            });
+            
+            const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = url;
             link.download = `${mediaSelecionado.legenda} - ${mediaSelecionado.titulo}`;
             link.click();
+
 
         } catch (error) {
             console.error('Erro ao fazer o download:', error);
@@ -178,7 +222,7 @@ export default function Reproducao({ mediaSelecionado, pausar, reproduzir, avanc
                 </NavItem>
             ) : (
                 <NavItem className={css(styles.item2)}>
-                    <Download className={css(styles.item21)} onClick={() => fazerDownloadProvisorio(mediaSelecionado)}/>
+                    <Download className={css(styles.item21)} onClick={() => fazerDownload(mediaSelecionado)}/>
                 </NavItem>
             )}
             
