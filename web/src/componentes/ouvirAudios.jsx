d40346e8345c29006291d6c0ced1910f56a43775
Aristides <harry.matoca@gmail.com>
2023-07-12 00:27:30 +0100

    audio

--- web/src/componentes/ouvirAudios.jsx
+++ web/src/componentes/ouvirAudios.jsx
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { StyleSheet, css } from 'aphrodite'
 import { MdPlayCircle as Play, MdPauseCircle as Pause } from "react-icons/md"
+import { FiHeart as Heart } from "react-icons/fi"
+import { BsThreeDots as Dots } from "react-icons/bs"
+import { BiLeftArrow as Arrow } from "react-icons/bi"
+
 
 const AudioPlayer = ({ audios, selecionarAudio, isPlaying, irPerfil }) => {
   const [audioPlayingIndex, setAudioPlayingIndex] = useState(null)
@@ -27,40 +31,111 @@ const AudioPlayer = ({ audios, selecionarAudio, isPlaying, irPerfil }) => {
     selecionarAudio(index, 1, playPause);
   };
 
+  //Funções de Configuranções------------------------------------------------------------------
+  const [showOptions, setShowOptions] = useState(false)
+  const iconRef = useRef(null)
+  const optionsRef = useRef(null)
+
+  const handleOutsideClick = (event) => {
+    if (
+      iconRef.current &&
+      !iconRef.current.contains(event.target) &&
+      optionsRef.current &&
+      !optionsRef.current.contains(event.target)
+    ) {
+      setShowOptions(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleOutsideClick);
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
+
+  const handleClick2 = () => {
+    setShowOptions(!showOptions)
+  };
+
+  const handleOptionsClick = () => {
+    setShowOptions(false)
+  };
+
+  const [hovered, setHovered] = useState(null);
+
+  const handleMouseEnter = (index) => {
+    setHovered(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(null);
+  };
+
   return (
     <div className={css(styles.container)}>
       <div className={css(styles.playlist)}>
-        <span style={{background: 'none'}}>{playlist}</span>
+        <span style={{ background: 'none' }}>{playlist}</span>
       </div>
-      <div className={css(styles.info)} style={{background: 'none'}}>
+      <div className={css(styles.info)} style={{ background: 'none' }}>
 
         <div className={css(styles.spans)}>
-          <span style={{background: 'none'}}>#</span>
-          <span style={{marginLeft: '4%', background: 'none'}}>Título</span>
+          <span style={{ background: 'none' }}>#</span>
+          <span style={{ marginLeft: '4%', background: 'none' }}>Título</span>
         </div>
 
-        <span style={{background: 'none', marginRight: '-1.5%'}}>Reproduzir</span>
+        <span style={{ background: 'none' }}>Reproduzir</span>
       </div>
       {audios.map((audio, index) => (
-        <div key={index} className={css(styles.audios)}>
+        <div key={index} className={css(styles.audios)} onMouseEnter={() => handleMouseEnter(index)} onMouseLeave={handleMouseLeave}>
 
           <div className={css(styles.spans)}>
-            <span style={{background: 'none'}}>{index+1}</span>
+            <span style={{ background: 'none' }}>{index + 1}</span>
             <img src={audio.imageDownloadURL} className={css(styles.img)} alt="" />
 
             <div className={css(styles.titles)}>
-              <span style={{background: 'none'}}>{audio.titulo}</span><br />
+              <span style={{ background: 'none' }}>{audio.titulo}</span><br />
               <span className={css(styles.artista)} onClick={() => irPerfil(audio.autor)}>{audio.autor}</span>
             </div>
           </div>
 
-          <span style={{background: 'none'}} onClick={() => handleClick(index)}>
-            {audioPlayingIndex === index && isPlaying == true ? (
-              <Pause className={css(styles.icone)}/>
+          <div style={{ background: 'none', width: '14%' }}>
+
+            {hovered === index ? (
+              <span className={css(styles.favorite)}>
+                <Heart style={{ background: 'none' }} />
+              </span>
             ) : (
-              <Play className={css(styles.icone)}/>
+              <span className={css(styles.favorite)}>
+                <Heart style={{ background: 'none', visibility: 'hidden' }} />
+              </span>
             )}
-          </span>
+
+            <span style={{ background: 'none' }} onClick={() => handleClick(index)}>
+              {audioPlayingIndex === index && isPlaying == true ? (
+                <Pause className={css(styles.icone)} />
+              ) : (
+                <Play className={css(styles.icone)} />
+              )}
+            </span>
+
+            <span className={css(styles.options)}>
+              {hovered === index ? (
+                <React.Fragment>
+                  <Dots style={{ background: 'none' }} ref={iconRef} onClick={handleClick2} />
+                  {showOptions ? (
+                    <div className={css(styles.definition)} onClick={handleOptionsClick} ref={optionsRef}>
+                      <p className={css(styles.cont)}>Adicionar aos favoritos</p>
+                      <p className={css(styles.cont)} onClick={() => irPerfil(audio.autor)}>Ir ao perfil do artista</p>
+                      <p className={css(styles.cont2)}><Arrow style={{background: 'none', color: 'white'}}/> Adicionar a playlist</p>
+                    </div>
+                  ) : null}
+                </React.Fragment>
+              ) : (
+                <Dots style={{ background: 'none', visibility: 'hidden' }} />
+              )}
+            </span>
+          </div>
         </div>
       ))}
     </div>
@@ -70,7 +145,7 @@ const AudioPlayer = ({ audios, selecionarAudio, isPlaying, irPerfil }) => {
 export default AudioPlayer;
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     borderTopLeftRadius: '8px',
     borderTopRightRadius: '8px',
     background: 'black',
@@ -78,7 +153,7 @@ const styles = StyleSheet.create({
     marginLeft: '-0.8%',
   },
 
-  playlist:{
+  playlist: {
     background: 'linear-gradient(to bottom, #555555, rgba(0, 0, 0, 0.3))',
     borderTopLeftRadius: '8px',
     borderTopRightRadius: '8px',
@@ -87,7 +162,7 @@ const styles = StyleSheet.create({
     color: 'white'
   },
 
-  info:{
+  info: {
     borderBottom: '1px solid grey',
     display: 'flex',
     justifyContent: 'space-between',
@@ -99,7 +174,7 @@ const styles = StyleSheet.create({
     margin: '0 1.5% 0 1.5%'
   },
 
-  audios:{
+  audios: {
     //border: '1px solid red',
     display: 'flex',
     justifyContent: 'space-between',
@@ -107,19 +182,19 @@ const styles = StyleSheet.create({
     color: 'white',
     background: 'black',
     fontSize: '16px',
-    padding: '0.3% 2% 0.3% 0',
+    padding: '0.3% 0 0.3% 0',
     margin: '0 1.5% 0 1.5%',
 
-    ':hover':{
+    ':hover': {
       background: 'rgb(36,36,36)',
       borderRadius: '6px'
     },
-    ':active':{
+    ':active': {
       background: 'rgb(36,36,36)'
     }
   },
 
-  spans:{
+  spans: {
     background: 'none',
     marginLeft: '1%',
     display: 'flex',
@@ -127,35 +202,92 @@ const styles = StyleSheet.create({
     width: '40%'
   },
 
-  img:{
+  img: {
     width: '45px',
     height: '40px',
     marginLeft: '4%',
     background: 'rgb(36,36,36)'
   },
 
-  titles:{
+  titles: {
     marginLeft: '3%',
     background: 'none',
     textAlign: 'justify'
   },
 
-  artista:{
+  artista: {
     background: 'none',
     fontSize: '14px',
 
-    ':hover':{
+    ':hover': {
       cursor: 'pointer',
       textDecoration: 'underline'
     }
   },
 
-  icone: {
-    background: 'none', 
-    fontSize: '29px',
-    ':hover':{
+  favorite: {
+    background: 'none',
+    fontSize: '22px',
+    ':hover': {
       cursor: 'pointer'
     }
-  }
+  },
+
+  icone: {
+    margin: '0 10% 0 25%',
+    background: 'none',
+    fontSize: '29px',
+    ':hover': {
+      cursor: 'pointer'
+    }
+  },
+
+  options: {
+    marginRight: '-12%',
+    background: 'none',
+    fontSize: '25px',
+    ':hover': {
+      cursor: 'pointer'
+    }
+  },
+
+  definition: {
+    position: 'absolute',
+    top: '53%',
+    right: '2%',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
+    textAlign: 'justify',
+    background: 'rgb(44,44,43)',
+    width: '17%'
+  },
+
+  cont: {
+    background: 'none',
+    fontSize: '18px',
+    color: 'white',
+    margin: '5px',
+    padding: '4px',
+    ':hover': {
+      cursor: 'pointer',
+      color: 'rgba(255, 213, 0, 1)',
+      background: 'rgb(36,36,36)'
+    },
+  },
+
+  cont2: {
+    borderTop: '2.5px solid rgb(36,36,36)',
+    background: 'none',
+    fontSize: '18px',
+    color: 'white',
+    margin: '5px',
+    padding: '4px',
+    ':hover': {
+      cursor: 'pointer',
+      color: 'rgba(255, 213, 0, 1)',
+      background: 'rgb(36,36,36)'
+    },
+  },
 })
 
